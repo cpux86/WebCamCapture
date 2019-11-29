be1eadccc224e8d2f115417dc9b2a1387f2beecd
Vladimir <cpux86@mail.ru>
2019-11-29 18:46:50 +0300

    Начало написания ОО кода

--- WebCamCapture/Form1.Designer.cs
+++ WebCamCapture/Form1.Designer.cs
@@ -30,14 +30,16 @@
         {
             this.btnVideoCapture = new System.Windows.Forms.Button();
             this.CamView = new System.Windows.Forms.PictureBox();
-            this.CamsList = new System.Windows.Forms.ListBox();
             this.btnScreenCapture = new System.Windows.Forms.Button();
+            this.btnSetting = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.CamList = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.CamView)).BeginInit();
             this.SuspendLayout();
             // 
             // btnVideoCapture
             // 
-            this.btnVideoCapture.Location = new System.Drawing.Point(452, 127);
+            this.btnVideoCapture.Location = new System.Drawing.Point(658, 54);
             this.btnVideoCapture.Name = "btnVideoCapture";
             this.btnVideoCapture.Size = new System.Drawing.Size(97, 23);
             this.btnVideoCapture.TabIndex = 0;
@@ -48,23 +50,16 @@
             // CamView
             // 
             this.CamView.BackColor = System.Drawing.Color.Black;
-            this.CamView.Location = new System.Drawing.Point(33, 26);
+            this.CamView.Location = new System.Drawing.Point(12, 6);
             this.CamView.Name = "CamView";
-            this.CamView.Size = new System.Drawing.Size(392, 395);
+            this.CamView.Size = new System.Drawing.Size(640, 480);
+            this.CamView.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.CamView.TabIndex = 1;
             this.CamView.TabStop = false;
             // 
-            // CamsList
-            // 
-            this.CamsList.FormattingEnabled = true;
-            this.CamsList.Location = new System.Drawing.Point(452, 26);
-            this.CamsList.Name = "CamsList";
-            this.CamsList.Size = new System.Drawing.Size(199, 95);
-            this.CamsList.TabIndex = 2;
-            // 
             // btnScreenCapture
             // 
-            this.btnScreenCapture.Location = new System.Drawing.Point(576, 127);
+            this.btnScreenCapture.Location = new System.Drawing.Point(771, 54);
             this.btnScreenCapture.Name = "btnScreenCapture";
             this.btnScreenCapture.Size = new System.Drawing.Size(75, 23);
             this.btnScreenCapture.TabIndex = 3;
@@ -72,17 +67,53 @@
             this.btnScreenCapture.UseVisualStyleBackColor = true;
             this.btnScreenCapture.Click += new System.EventHandler(this.BtnScreenCapture_Click);
             // 
+            // btnSetting
+            // 
+            this.btnSetting.Location = new System.Drawing.Point(771, 92);
+            this.btnSetting.Name = "btnSetting";
+            this.btnSetting.Size = new System.Drawing.Size(75, 23);
+            this.btnSetting.TabIndex = 4;
+            this.btnSetting.Text = "Настройка";
+            this.btnSetting.UseVisualStyleBackColor = true;
+            this.btnSetting.Click += new System.EventHandler(this.BtnSetting_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DisplayMember = "5";
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(791, 6);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(97, 21);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            // 
+            // CamList
+            // 
+            this.CamList.DisplayMember = "5";
+            this.CamList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.CamList.FormattingEnabled = true;
+            this.CamList.Location = new System.Drawing.Point(658, 6);
+            this.CamList.Name = "CamList";
+            this.CamList.Size = new System.Drawing.Size(127, 21);
+            this.CamList.TabIndex = 6;
+            this.CamList.SelectedIndexChanged += new System.EventHandler(this.CamList_SelectedIndexChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(898, 498);
+            this.Controls.Add(this.CamList);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnSetting);
             this.Controls.Add(this.btnScreenCapture);
-            this.Controls.Add(this.CamsList);
             this.Controls.Add(this.CamView);
             this.Controls.Add(this.btnVideoCapture);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "WebCamCapture";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.CamView)).EndInit();
             this.ResumeLayout(false);
 
@@ -92,8 +123,10 @@
 
         private System.Windows.Forms.Button btnVideoCapture;
         private System.Windows.Forms.PictureBox CamView;
-        private System.Windows.Forms.ListBox CamsList;
         private System.Windows.Forms.Button btnScreenCapture;
+        private System.Windows.Forms.Button btnSetting;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox CamList;
     }
 }
 
--- WebCamCapture/Form1.cs
+++ WebCamCapture/Form1.cs
@@ -6,23 +6,122 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
 
 namespace WebCamCapture
 {
     public partial class Form1 : Form
     {
+        private FilterInfoCollection videoDevices;
+        private VideoCaptureDevice videoSource;
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // инициализация, проверка наличия подключенных камер.
+            //init();
+
+            videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            if (videoDevices.Count > 0)
+            {
+                foreach (FilterInfo device in videoDevices)
+                {
+                    CamList.Items.Add(device.Name);
+                }
+                CamList.SelectedIndex = 0;
+            }
+        }
+
         private void BtnVideoCapture_Click(object sender, EventArgs e)
         {
-            CamsList.Items.Add("kdfkf");
+            videoSource = new VideoCaptureDevice(videoDevices[CamList.SelectedIndex].MonikerString);
+            videoSource.NewFrame += new NewFrameEventHandler(video_NewFrame);
+            VideoCapabilities[] capabilities = videoSource.VideoCapabilities;
+
+            foreach (var item in videoSource.VideoCapabilities)
+            {
+                comboBox1.Items.Add(item.FrameSize.Width + "x" + item.FrameSize.Height);
+            }
+            //videoSource.VideoResolution = capabilities[17]; // устанавливаем разрешение 1280x720
+            videoSource.Start();
+            
         }
 
         private void BtnScreenCapture_Click(object sender, EventArgs e)
+        {
+            CamView.Image.Save(DateTime.Now.ToString().Replace(":", "-")+".jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+        }
+
+        private void video_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Invoke((MethodInvoker)(() =>
+                {
+                    var image = CamView.Image;
+
+                    if (image != null)
+                    {
+                        CamView.Image = null;
+                        image.Dispose();
+                    }
+                    CamView.Image = (Bitmap)eventArgs.Frame.Clone();
+                }));
+
+
+        }
+
+ 
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Invoke((MethodInvoker)(() =>
+            {
+                if (videoSource != null)
+                {
+                    videoSource.SignalToStop();
+                    videoSource.WaitForStop();
+                }
+            }));
+        }
+
+        private void BtnSetting_Click(object sender, EventArgs e)
+        {
+            
+            foreach (var item in videoSource.VideoCapabilities)
+            {
+                comboBox1.Items.Add(item.FrameSize.Width+"x"+ item.FrameSize.Width);
+            }
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Invoke((MethodInvoker)(() =>
+            {
+                if (videoSource != null)
+                {
+                    videoSource.SignalToStop();
+                    videoSource.WaitForStop();
+                }
+            }));
+
+            VideoCapabilities[] capabilities = videoSource.VideoCapabilities;
+            videoSource.VideoResolution = capabilities[comboBox1.SelectedIndex];           
+            videoSource.Start();
+
+        }
+
+        private void CamList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class WebCamCapture {
+        public void init()
         {
 
         }
